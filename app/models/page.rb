43871e1add32e6485b8cfaffe101260fc27fa0a9
Scott Chacon <email@email.com>
2009-01-30 14:12:13 +0100

    save pages in yml

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,9 @@ require 'git'
 
 class Page < ActiveRecord::BaseWithoutTable # < ActiveRecord::Base
 
-  attr_accessor :permalink, :content, :name, :id
+  PAGES_PATH = "./app/views/pages/"
+
+  attr_accessor :permalink, :content, :name
 
   column :permalink, :string
   column :name, :string
@@ -11,17 +13,11 @@ class Page < ActiveRecord::BaseWithoutTable # < ActiveRecord::Base
 
   validates_presence_of :name, :permalink
 
-  #  versioning(:content) do |version|
-  #    version.repository = '/Users/amoore/Sites/rails/pages/.git'
-  #    version.message = lambda { |page| "Change to #{page.permalink}" }
-  #  end
-
   def self.all
     Dir.entries(pages_path).collect { |f|
-      dir = File.join(pages_path, f) unless f == '.' || f == '..'
-      dir if dir && File.directory?(dir)
-    }.compact.collect { |dir|
-      Page.new(:permalink => File.basename(dir))
+      f if File.fnmatch?('*.yml', f)
+    }.compact.collect { |f|
+      page = self.load_page_by_permalink(File.basename(f, '.yml'))
     }
   end
 
@@ -30,17 +26,43 @@ class Page < ActiveRecord::BaseWithoutTable # < ActiveRecord::Base
     page.new_record? ? nil : page
   end
 
+  def self.load_page_by_permalink(permalink)
+    if page_path = File.join(pages_path, permalink + '.yml')
+      File.file?(page_path) && File.open(page_path, 'r'){ |f|
+        YAML::load(f.read)
+      }
+    end
+  end
+
+  def load_page
+    page_path = File.join(PAGES_PATH, self.permalink + ".yml")
+    if page_path && File.file?(page_path)
+      File.open(page_path, 'r'){ |f|
+        if page = YAML::load(f.read)
+          self.content = page.content
+          self.name = page.name
+        end
+      }
+    end
+  end
+
+  def self.pages_path
+    PAGES_PATH
+  end
+
   def initialize(attributes = {})
     if attributes
       if permalink = attributes[:permalink]
         self.permalink = permalink
-        self.content = read_file("content.text.textile")
-        self.name = read_file("name.text.textile")
-        self.id = page_path.hash.abs if File.directory? page_path
+        load_page
       end
       self.content = attributes[:content] if attributes[:content]
       self.name = attributes[:name] if attributes[:name]
     end
+  end
+
+  def id
+    permalink.hash.abs
   end
 
   def to_param
@@ -52,65 +74,35 @@ class Page < ActiveRecord::BaseWithoutTable # < ActiveRecord::Base
   end
 
   def exists?
-    page_path && File.directory?(page_path)
+    page_path && File.file?(page_path)
   end
 
   def save
-    return false if File.file? page_path # todo file exists with that name, add to errors
+    # todo page exists with that permalink, add to errors
 
-    unless File.exists?(page_path)
-      begin
-        Dir.mkdir(page_path)
-        
-      rescue SystemCallError
-        return false  # todo directory couldn't be created, add to errors
-      end
-    end
+    write_file(page_path, self.to_yaml)
 
-    Dir.open(page_path) do |dir|
-      write_file(File.join(dir.path, "content.text.textile"), content)
-      write_file(File.join(dir.path, "name.text.textile"), name)
-
-      g = git
-      g.add(page_path)
-      g.commit("Saving #{permalink}")
-      true
-    end
+    #      g = git
+    #      g.add(page_path)
+    #      g.commit("Saving #{permalink}")
   end
 
   def destroy
-    destroy_dir
-    g = git
-    g.remove(page_path)
-    g.commit("Removing #{permalink}")
+    File.delete(page_path)
+#    g = git
+    #    g.remove(page_path)
+    #    g.commit("Removing #{permalink}")
     true
   end
-  
+
   private
 
-  def self.pages_path
-    "./app/views/pages/"
-  end
-
   def page_path
-    @page_dir ||= !permalink.nil? && File.join(Page.pages_path, permalink)
+    !permalink.nil? && File.join(Page.pages_path, permalink + ".yml")
   end
 
   def write_file(filepath, content)
     File.open(filepath, 'w') {|f| f.write(content) }
-  end
-
-  def read_file(basename)
-    filepath = File.join(page_path, basename)
-    s = ''
-    File.exists?(filepath) && File.open(filepath, 'r') do |f|
-      s = f.read
-    end
-    s
-  end
-
-  def destroy_dir
-    FileUtils.remove_dir(page_path) if File.directory? page_path
   end
 
   def git
